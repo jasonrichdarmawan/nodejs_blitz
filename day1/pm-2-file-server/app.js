51f0da300c961412add6a75f14a31c18b9eb74ea
Jason <jason.onggo@tempatkerja.com>
2023-01-24 09:00:27 +0700

    update delete and rename

--- day1/pm-2-file-server/app.js
+++ day1/pm-2-file-server/app.js
@@ -2,13 +2,14 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
+    // reads the HTML file, and return the content
     fs.readFile('demofile1.html', function(err, data) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(data);
         return res.end();
     });
 
-    // create a file named mynewfile1.txt
+    // append Hello content! to mynewfile1.txt
     fs.appendFile('mynewfile1.txt', 'Hello content!', function(err) {
         if (err) throw err;
         console.log('Saved!');
@@ -18,5 +19,22 @@ http.createServer(function (req, res) {
     fs.open('mynewfile2.txt', 'w', function(err, file) {
         if (err) throw err;
         console.log('Saved!');
+    })
+
+    // create a file named mynewfile3.txt
+    fs.writeFile('mynewfile3.txt', 'Hello content!', function(err) {
+        if (err) throw err;
+        console.log('Saved!');
+    })
+
+    // delete mynewfile1.txt
+    fs.unlink('mynewfile1.txt', function (err) {
+        if (err) throw err;
+        console.log('File deleted!');
+    })
+
+    fs.rename('mynewfile2.txt', 'mynewfile4.txt', function (err) {
+        if (err) throw err;
+        console.log('File Renamed!');
     })
 }).listen(8080);
